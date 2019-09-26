09368468187ce4a0ff35fb5660ef33c6b759f92e
Sam <samroman3@gmail.com>
2019-09-26 12:48:30 -0400

    Adds customcolelctioviewcell and adds outlets for collectionview and cell to viewcontroller and collection cell respectively

---
+++ collectionViewsLab/collectionViewsLab/CountryCollectionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CountryCollectionViewCell.swift
+//  collectionViewsLab
+//
+//  Created by Sam Roman on 9/26/19.
+//  Copyright © 2019 Sam Roman. All rights reserved.
+//
+
+import UIKit
+
+class CountryCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var countryCell: UIView!
+    
+    @IBOutlet weak var name: UILabel!
+    
+    @IBOutlet weak var population: UILabel!
+    
+    @IBOutlet weak var capital: UILabel!
+    
+    
+    
+}
--- collectionViewsLab/collectionViewsLab/ViewController.swift
+++ collectionViewsLab/collectionViewsLab/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countryCollection: UICollectionView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
